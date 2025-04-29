a2c36b1ab9ddf3ce8b77ba529327efa2e2cb9f99
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-29 15:19:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const list = document.getElementById('infi-list')
+let  count = 10;
 
+for (let i = 1; i <= count; i++) {
+  const li = document.createElement('li');
+  li.textContent = `Item ${i}`;
+  list.appendChild(li);
+}
+function addItems(num = 2) {
+  for (let i = 0; i < num; i++) {
+    count++;
+    const li = document.createElement('li');
+    li.textContent = `Item ${count}`;
+    list.appendChild(li);
+  }
+}
+list.addEventListener('scroll', () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems();
+  }
+});
